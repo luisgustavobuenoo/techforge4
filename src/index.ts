8be04ec6f424b156763b6a089a957e4991ea564b
luisgustavobuenoo <161783200+luisgustavobuenoo@users.noreply.github.com>
2024-11-01 18:18:50 -0300

    atividade techforge 4

--- src/index.ts
+++ src/index.ts
@@ -18,3 +18,85 @@ class ContaBancaria {
         }
     }
 }
+
+
+
+
+// Exercício 2
+class Livro {
+    titulo: string;
+    autor: string;
+    paginas: number;
+    lido: boolean;
+
+    constructor(titulo: string, autor: string, paginas: number, lido: boolean = false) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.paginas = paginas;
+        this.lido = lido;
+    }
+
+    marcarComoLido(): void {
+        this.lido = true;
+    }
+}
+
+
+
+
+// Exercício 3
+class Produto {
+    nome: string;
+    preco: number;
+    quantidade: number;
+
+    constructor(nome: string, preco: number, quantidade: number) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    calcularValorEstoque(): number {
+        return this.preco * this.quantidade;
+    }
+}
+
+
+
+
+// Exercício 4
+class Temperatura {
+    valor: number;
+
+    constructor(valor: number) {
+        this.valor = valor;
+    }
+
+    paraFahrenheit(): number {
+        return this.valor * 9 / 5 + 32;
+    }
+
+    paraKelvin(): number {
+        return this.valor + 273.15;
+    }
+}
+
+
+
+
+// Exercício 5
+class Agenda {
+    compromissos: string[];
+
+    constructor() {
+        this.compromissos = [];
+    }
+
+    adicionarCompromisso(compromisso: string): void {
+        this.compromissos.push(compromisso);
+    }
+
+    listarCompromissos(): string[] {
+        return this.compromissos;
+    }
+}
